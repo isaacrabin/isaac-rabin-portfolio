3eae8fefd0671c312e31d8589a3c8e08e38fec73
isaacrabin <isaacmrongo@gmail.com>
2026-05-22 12:58:18 +0300

    fix:updated failing deploy workflow

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'export', 
   images: {
     remotePatterns: [
       {
@@ -13,6 +14,7 @@ const nextConfig: NextConfig = {
   basePath: process.env.NODE_ENV === 'production' ? '/isaac-rabin-portfolio' : '',
   assetPrefix: process.env.NODE_ENV === 'production' ? '/isaac-rabin-portfolio/' : '',
   trailingSlash: true,
+  distDir: 'out',
 };
 
 export default nextConfig;
